f88840d86477b5e1337c68f16444ac0a23e9e723
SkaDigitalHub <skadigitalhub@gmail.com>
2025-06-07 10:08:57 +0000

    Update agent.js

--- script/agent.js
+++ script/agent.js
@@ -400,4 +400,11 @@
             document.querySelector('.mode-toggle').classList.add('light');
         }
  
- 
+function downloadFile() {
+  const link = document.createElement('a');
+  link.href = 'downloads/pdf/timetable1.pdf';
+  link.download = 'tmsom1stsem-timetable.pdf'; // Custom filename
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
